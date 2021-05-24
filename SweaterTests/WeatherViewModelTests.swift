229b19b91a22b8e9d4936adb1bdb53ac32e44324
Bob Warwick <bob@wholepunk.com>
2021-05-23 21:39:49 -0700

    Framing out some tests

---
+++ SweaterTests/WeatherViewModelTests.swift
@@ -0,0 +1,40 @@
+//
+//  WeatherViewModelTests.swift
+//  SweaterTests
+//
+//  Created by Bob Warwick on 2021-05-23.
+//
+
+import XCTest
+
+class WeatherViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+        // Clear out the existing data model
+        
+        // Add in a basic data model for one 'current location' and one city that has filled in values
+        
+    }
+
+    func testSelectCity() {
+        
+        // Select the city
+        
+        // Check to make sure the selected index is correct
+        
+    }
+    
+    func testLocationViewModelsCreated() {
+        
+        // Check to make sure we have the location view controllers we expect to have
+        
+    }
+    
+    func testDeleteLocation() {
+        
+        // Delete one of the location view models and make sure it dissapears from our list
+        
+    }
+    
+}
