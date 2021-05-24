fe7302e9f16ce29dc4269e7aecbf9c0d2e6854f0
Bob Warwick <bob@wholepunk.com>
2021-05-24 02:23:45 -0700

    Added a couple really basic unit tests.

--- SweaterTests/SweaterTests.swift
+++ SweaterTests/SweaterTests.swift
@@ -17,5 +17,35 @@ class SweaterTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    // A basic test of the weather viewmodel to make sure that it reflects the data we see in the model
+    func testWeatherViewModel() throws {
+        
+        MockDataManager.shared.loadMockData()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let persistentContainer = appDelegate.persistentContainer
+        
+        let viewModel = WeatherViewModel(withPersistentContainer: persistentContainer)
+        
+        XCTAssert(viewModel.selectedCardIndex == 0, "We started with the wrong selected index")
+        XCTAssert(viewModel.locationViewModels.count == 3, "There's something out of whack with the location view models")
+        
+    }
+
+    // A basic test of a location viewmodel to make sure it's getting instantiated and has some visibility on the data
+    func testLocationViewModel() throws {
+        
+        MockDataManager.shared.loadMockData()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let persistentContainer = appDelegate.persistentContainer
+        
+        let weatherViewModel = WeatherViewModel(withPersistentContainer: persistentContainer)
+        let locationsViewModel = weatherViewModel.locationViewModels[1]
+        
+        XCTAssert(locationsViewModel.cityName == "Vancouver")
+        
+    }
 
 }
